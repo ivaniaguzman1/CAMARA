55f6ad9c4efa6f53f6a9bdf91b2cce5d7e08e50b
MINEDUCYT <MINEDUCYT@DESKTOP-NFDMETJ>
2023-03-18 09:33:21 -0300

    Initial commit

---
+++ app/src/main/java/com/ugb/camara/MainActivity.java
@@ -0,0 +1,53 @@
+package com.ugb.camara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button idbtncamara;
+    ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        idbtncamara = findViewById(R.id.idbtncamara);
+        imageView2 = findViewById(R.id.imageView2);
+
+        idbtncamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCamara();
+
+            }
+        });
+
+    }
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent,1);
+        //}
+}
+
+         protected void onActivityResult(int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 1 && resultCode == RESULT_OK){
+                Bundle extras = data.getExtras();
+                Bitmap imgBitmap = (Bitmap) extras.get("data");
+                imageView2.setImageBitmap(imgBitmap);
+
+
+            }
+         }
+
+}
